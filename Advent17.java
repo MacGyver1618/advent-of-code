8683e2e4132e5e5475682fb27e1f80943b2985b5
Joni Malén <macgyver1618@gmail.com>
2018-12-18 18:49:52 +0200

    Cleaned up Day 17

--- Advent17.java
+++ Advent17.java
@@ -4,253 +4,186 @@ import java.util.stream.*;
 
 public class Advent17 extends Advent {
 
-  Pattern xVein = Pattern.compile("x=(\\d+), y=(\\d+)..(\\d+)");
-  Pattern yVein = Pattern.compile("y=(\\d+), x=(\\d+)..(\\d+)");
+  Pattern pattern = Pattern.compile("([xy])=(\\d+), [xy]=(\\d+)..(\\d+)");
 
-  int Y_MIN = Integer.MAX_VALUE;
-  int Y_MAX = Integer.MIN_VALUE;
-  int X_MIN = 500;
-  int X_MAX = 500;
+  Map<Point, Character> grid = new TreeMap<>();
+
+  int xMin = 500;
+  int xMax = 500;
+  int yMin = Integer.MAX_VALUE;
+  int yMax = Integer.MIN_VALUE;
 
   Point spring = new Point(500,0);
-  Set<Point> cursors = new TreeSet<>();
-  Map<Point, Character> ground = new TreeMap<>(Point.yComparator());
+  Set<Point> drips = new TreeSet<>();
+  Set<Point> newDrips = new TreeSet<>();
+  Set<Point> deadDrips = new TreeSet<>();
 
   public Advent17() {
     super(17);
   }
 
   @Override
-  protected void readInput() {
-    //readExample();
-    super.readInput();
-  }
-
-  @Override
   protected void parseInput() {
-    ground.clear();
-    ground.put(spring, '+');
+    grid.put(spring, '+');
     for (String line : input) {
-      Matcher xMatcher = xVein.matcher(line);
-      Matcher yMatcher = yVein.matcher(line);
-      if (xMatcher.find()) {
-        populateVein(xMatcher, 'X');
-      } else if (yMatcher.find()) {
-        populateVein(yMatcher, 'Y');
+      Matcher m = pattern.matcher(line);
+      m.find();
+      char first = m.group(1).charAt(0);
+      int a = Integer.parseInt(m.group(2));
+      int b = Integer.parseInt(m.group(3));
+      int c = Integer.parseInt(m.group(4));
+      if (first == 'x') {
+        parseVein(a, a, b, c);
+      } else {
+        parseVein(b, c, a, a);
       }
     }
   }
 
-  private void populateVein(Matcher m, char dim) {
-    int dConst = Integer.parseInt(m.group(1));
-    int dMin = Integer.parseInt(m.group(2));
-    int dMax = Integer.parseInt(m.group(3));
-    if (dim == 'X') {
-      drawVein(dConst, dConst, dMin, dMax);
-    } else {
-      drawVein(dMin, dMax, dConst, dConst);
-    }
-  }
-
-  private void drawVein(int xStart, int xEnd, int yStart, int yEnd) {
-    for (int y = yStart; y <= yEnd; y++) {
-      if (y < Y_MIN) Y_MIN = y;
-      if (y > Y_MAX) Y_MAX = y;
-      for (int x = xStart; x <= xEnd; x++) {
-        if (x < X_MIN) X_MIN = x;
-        if (x > X_MAX) X_MAX = x;
+  private void parseVein(int x0, int x1, int y0, int y1) {
+    for (int x = x0; x <= x1; x++) {
+      if (x > xMax) xMax = x;
+      if (x < xMin) xMin = x;
+      for (int y = y0; y <= y1; y++) {
+        if (y > yMax) yMax = y;
+        if (y < yMin) yMin = y;
         Point p = new Point(x,y);
-        ground.put(p, '#');
+        grid.put(p, '#');
       }
     }
   }
 
   @Override
   protected String part1() {
-    sopl("X=", X_MIN, "..", X_MAX, ", Y=", Y_MIN, "..", Y_MAX);
-    //printGround(0,81);
-    cursors.add(spring);
-    int tick = 0;
-    int start = 0, end = 81;
-    String command = "";
-    while (cursorsInRange(cursors)) {
-      tick++;
-      cursors = tick();
+    long stillWater = 0;
+    long previousStill = Integer.MIN_VALUE;
+    long flowingWater = 0;
+    long previousFlowing = Integer.MIN_VALUE;
+    drips.add(spring);
+    while (stillWater != previousStill || flowingWater != previousFlowing) {
+      drips.addAll(newDrips);
+      drips.removeAll(deadDrips);
+      newDrips.clear();
+      deadDrips.clear();
+      for (Point drip : drips) {
+        drip(drip);
+      }
+      previousStill = stillWater;
+      stillWater = stillWater();
+      previousFlowing = flowingWater;
+      flowingWater = flowingWater();
     }
     return "" + countWater();
   }
 
   private long countWater() {
-    return ground.entrySet()
-                 .stream()
-                 .filter(e -> e.getKey().y >= Y_MIN && e.getKey().y <= Y_MAX)
-                 .map(e -> e.getValue())
-                 .filter(c -> c == '~' || c == '|')
-                 .count();
+    return stillWater() + flowingWater();
   }
 
-  private boolean cursorsInRange(Set<Point> cursors) {
-    return cursors.stream().anyMatch(p -> p.y <= Y_MAX);
+  private long stillWater() {
+    return countWater('~');
   }
 
-  private Set<Point> tick() {
-    Set<Point> result = new TreeSet<>();
-    cursors.forEach(c -> result.addAll(tick(c)));
-    return result;
+  private long flowingWater() {
+    return countWater('|');
   }
 
-  private Set<Point> tick(Point cursor) {
-    Set<Point> result = new TreeSet<>(Point.yComparator().reversed());
-    if (cursor.x > X_MAX) X_MAX = cursor.x;
-    if (cursor.x < X_MIN) X_MIN = cursor.x;
-    Point down = down(cursor);
-    if (down.y > Y_MAX) return result;
-    char below = ground.getOrDefault(down, '.');
-    if (below == '.') {
-      ground.put(down, '|');
-      result.add(down);
-    } else if (below == '|') {
-      ground.put(cursor, '|');
-    } else if (overFlow(down)) {
-      ground.put(cursor, '|');
-    } else if (below == '#'|| below == '~') {
-      Set<Point> horizontalCursors = spreadHorizontal(cursor);
-      if (horizontalCursors.isEmpty()) {
-        result.addAll(findFlowPoints(cursor));
-      } else {
-        result.addAll(horizontalCursors);
+  private long countWater(char match) {
+    return grid.entrySet()
+               .parallelStream()
+               .filter(e -> e.getKey().y >= yMin)
+               .filter(e -> e.getValue() == match)
+               .count();
+  }
+
+  private void drip(Point drip) {
+    if (below(drip) == '~') {
+      deadDrips.add(drip);
+      return;
+    }
+    Point cursor = drip;
+    while (below(cursor) != '#' && below(cursor) != '~' ) {
+      if (cursor.y > yMax) {
+        return;
       }
+      if (at(cursor) == '.') {
+        grid.put(cursor, '|');
+      }
+      cursor = down(cursor);
     }
-    return result;
-  }
-
-  private boolean overFlow(Point cursor) {
-    return overFlowLeft(left(cursor)) || overFlowRight(right(cursor));
-  }
-
-  private boolean overFlowLeft(Point cursor) {
-    if (cursor.x < X_MIN) return false;
-    char c = ground.getOrDefault(cursor, '.');
-    if (c == '#') return false;
-    return c == '|' || overFlowLeft(left(cursor));
-  }
-
-  private boolean overFlowRight(Point cursor) {
-    if (cursor.x > X_MAX) return false;
-    char c = ground.getOrDefault(cursor, '.');
-    if (c == '#') return false;
-    return c == '|' || overFlowRight(right(cursor));
-  }
-
-  private Set<Point> findFlowPoints(Point cursor) {
-    Set<Point> result = new TreeSet<>();
-    Point candidate = up(cursor);
-    char atCandidate = ground.getOrDefault(candidate, '.');
-    if (atCandidate == '|') {
-      result.add(candidate);
-    }
-    result.addAll(flowLeft(candidate));
-    result.addAll(flowRight(candidate));
-    return result;
-  }
-
-  private Set<Point> flowLeft(Point p) {
-    Set<Point> result = new TreeSet<>();
-    Point candidate = left(p);
-    if (candidate.x < X_MIN) return result;
-    char atCandidate = ground.getOrDefault(candidate, '.');
-    if (atCandidate == '#') {
-      return result;
-    }
-    if (atCandidate == '|') {
-      result.add(candidate);
-    }
-    result.addAll(flowLeft(candidate));
-    return result;
-  }
-
-  private Set<Point> flowRight(Point p) {
-    Set<Point> result = new TreeSet<>();
-    Point candidate = right(p);
-    if (candidate.x > X_MAX) return result;
-    char atCandidate = ground.getOrDefault(candidate, '.');
-    if (atCandidate == '#') {
-      return result;
-    }
-    if (atCandidate == '|') {
-      result.add(candidate);
-    }
-    result.addAll(flowRight(candidate));
-    return result;
-  }
-
-  private Set<Point> spreadHorizontal(Point p) {
-    Set<Point> result = new TreeSet<>();
-    ground.put(p, '~');
-    result.addAll(spreadLeft(left(p)));
-    result.addAll(spreadRight(right(p)));
-    return result;
-  }
-
-  private Set<Point> spreadLeft(Point p) {
-    Set<Point> result = new TreeSet<>();
-    char current = ground.getOrDefault(p, '.');
-    if (current == '#') return result;
-    char below = ground.getOrDefault(down(p), '.');
-    if (below == '.') {
-      ground.put(p, '|');
-      setOverFlow(p);
-      result.add(p);
-    } else if (below == '|') {
-      ground.put(p, '|');
+    Point le = edgeToLeft(cursor);
+    Point re = edgeToRight(cursor);
+    if (at(le) == '#' && at(re) == '#') {
+      fill(right(le), re);
     } else {
-      if (current != '|') {
-        ground.put(p, '~');
-      }
-      result.addAll(spreadLeft(left(p)));
+      if (at(le) == '#') le = right(le);
+      if (at(re) != '#') re = right(re);
+      overFlow(le, re);
     }
-    return result;
   }
 
-  private Set<Point> spreadRight(Point p) {
-    Set<Point> result = new TreeSet<>();
-    char current = ground.getOrDefault(p, '.');
-    if (current == '#') return result;
-    char below = ground.getOrDefault(down(p), '.');
-    if (below == '.') {
-      ground.put(p, '|');
-      setOverFlow(p);
-      result.add(p);
-    } else if (below == '|') {
-      ground.put(p, '|');
-    } else {
-      if (current != '|') {
-        ground.put(p, '~');
-      }
-      result.addAll(spreadRight(right(p)));
+  private Point edgeToLeft(Point p) {
+    Point left = left(p);
+    while (!atEdge(left)) {
+      left = left(left);
+      if (left.x < xMin) xMin = left.x;
     }
-    return result;
+    return left;
   }
 
-  private void setOverFlow(Point p) {
-    setOverFlowLeft(left(p));
-    setOverFlowRight(right(p));
+  private Point edgeToRight(Point p) {
+    Point right = right(p);
+    while (!atEdge(right)) {
+      right = right(right);
+      if (right.x > xMax) xMax = right.x;
+    }
+    return right;
   }
 
-  private void setOverFlowLeft(Point p) {
-    char current = ground.getOrDefault(p, '.');
-    char below = ground.getOrDefault(down(p), '.');
-    if (below == '|' || current == '#' || below == '.') return;
-    ground.put(p, '|');
-    setOverFlowLeft(left(p));
+  private boolean atEdge(Point p) {
+    return below(p) == '.' || below(p) == '|' || at(p) == '#';
   }
 
-  private void setOverFlowRight(Point p) {
-    char current = ground.getOrDefault(p, '.');
-    char below = ground.getOrDefault(down(p), '.');
-    if (below == '|' || current == '#' || below == '.') return;
-    ground.put(p, '|');
-    setOverFlowRight(right(p));
+  private void fill(Point start, Point end) {
+    Point cursor = start;
+    while (!cursor.equals(end)) {
+      grid.put(cursor, '~');
+      cursor = right(cursor);
+    }
+  }
+
+  private void overFlow(Point start, Point end) {
+    Point cursor = start;
+    if (below(start) == '.') {
+      newDrips.add(start);
+    }
+    if (below(left(end)) == '.') {
+      newDrips.add(left(end));
+    }
+    while (!cursor.equals(end)) {
+      grid.put(cursor, '|');
+      cursor = right(cursor);
+    }
+  }
+
+  private char above(Point p) {
+    return at(up(p));
+  }
+
+  private char below(Point p) {
+    return at(down(p));
+  }
+
+  private char leftOf(Point p) {
+    return at(left(p));
+  }
+
+  private char rightOf(Point p) {
+    return at(right(p));
+  }
+
+  private char at(Point p) {
+    return grid.getOrDefault(p, '.');
   }
 
   private Point up(Point p) {
@@ -269,69 +202,8 @@ public class Advent17 extends Advent {
     return p.add(new Point(1,0));
   }
 
-  private void printGround() {
-    printGround(Y_MIN, Y_MAX);
-  }
-
-  private void printGround(int yStart, int yEnd) {
-    StringBuilder sb = new StringBuilder();
-    for (int y = yStart; y <= yEnd; y++) {
-      for (int x = X_MIN; x <= X_MAX; x++) {
-        Point p = new Point(x,y);
-        char c = ground.getOrDefault(p, ' ');
-        sb.append(c);
-      }
-      sb.append('\n');
-    }
-    sop(sb.toString());
-  }
-
   @Override
   protected String part2() {
-    int tick = 0;
-    int start = 0;
-    int end = 82;
-    while (tick < 0) {
-      printStillGround(Math.max(start, 0), Math.min(end,Y_MAX));
-      sopl("Tick ", tick, ": ", cursors);
-      switch(readString()) {
-        case "p":
-          start -= 83;
-          end -= 83;
-          break;
-        case "n":
-          start += 83;
-          end += 83;
-          break;
-        case "q":
-          tick = 5001;
-      }
-    }
-    printStillGround(0,Y_MAX);
-    return "" + countStillWater();
-  }
-
-  private void printStillGround(int yStart, int yEnd) {
-    sopl(stillGround(yStart, yEnd));
-  }
-
-  private String stillGround(int yStart, int yEnd) {
-    StringBuilder sb = new StringBuilder();
-    for (int y = yStart; y <= yEnd; y++) {
-      for (int x = X_MIN; x <= X_MAX; x++) {
-        Point p = new Point(x,y);
-        char c = ground.getOrDefault(p, ' ');
-        sb.append(c == '|' ? ' ' : c);
-      }
-      sb.append('\n');
-    }
-    return sb.toString();
-  }
-
-  private long countStillWater() {
-    return ground.values()
-                 .stream()
-                 .filter(c -> c == '~')
-                 .count();
+    return "" + stillWater();
   }
 }
