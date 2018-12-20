9955f977442cd038cedc25568be6e4dcb5880226
Joni Malén <macgyver1618@gmail.com>
2018-12-20 08:32:28 +0200

    Cleaned up solution

--- Advent20.java
+++ Advent20.java
@@ -12,54 +12,30 @@ public class Advent20 extends Advent {
   }
 
   @Override
-  protected void readInput() {
-    //readExample();
-    super.readInput();
-  }
-
-  @Override
   protected void parseInput() {
     regex = input.get(0);
   }
 
   @Override
   protected String part1() {
-    int i = 1;
-    char c = regex.charAt(i);
-    String s = "";
-    Stack<String> stack = new Stack<>();
     Point cursor = new Point(0,0);
     Stack<Point> cursors = new Stack<>();
     int pathLength = 0;
     Stack<Integer> pathLengths = new Stack<>();
-    while (c != '$') {
+    int i = 0;
+    for (char c = regex.charAt(++i); c != '$'; c = regex.charAt(++i)) {
       switch (c) {
         case '(':
-          stack.push(s);
-          stack.push("(");
           cursors.push(cursor);
           pathLengths.push(pathLength);
-          s = "";
           break;
         case '|':
-          if (stack.peek().equals("(")) {
-            stack.push(s);
-            s = "";
-          } else {
-            String old = stack.pop();
-            stack.push(old.length() > s.length() ? old : s);
-          }
           cursor = cursors.peek();
           pathLength = pathLengths.peek();
           break;
         case ')':
-          String old = stack.pop();
-          stack.pop(); // Opening paren
-          String base = stack.pop();
-          base += old.length() > s.length() ? old : s;
           pathLength = pathLengths.pop();
           cursor = cursors.pop();
-          s = base;
           break;
         default:
           switch (c) {
@@ -81,9 +57,7 @@ public class Advent20 extends Advent {
           if (pathLength < oldShortest) {
             pathsToRooms.put(cursor, pathLength);
           }
-          s += c;
       }
-      c = regex.charAt(++i);
     }
     return "" + longestShortestPath();
   }
