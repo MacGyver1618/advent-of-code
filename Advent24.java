ceb3cc71c88042050e53c6b1ed0800e6cce70a20
Joni Malén <macgyver1618@gmail.com>
2018-12-24 14:52:10 +0200

    Finished Day 24

--- Advent24.java
+++ Advent24.java
@@ -15,18 +15,15 @@ public class Advent24 extends Advent {
                                                 .thenComparingInt((Group g) -> g.initiative)
                                                 .reversed();
 
+  int boost = 0;
+
   public Advent24() {
     super(24);
   }
 
   @Override
-  protected void readInput() {
-    //readExample();
-    super.readInput();
-  }
-
-  @Override
   protected void parseInput() {
+    groups.clear();
     String faction = null;
     for (String line : input) {
       if (line.endsWith(":")) {
@@ -46,7 +43,9 @@ public class Advent24 extends Advent {
     group.faction = faction;
     group.units = Integer.parseInt(m.group(1));
     group.hp = Integer.parseInt(m.group(2));
-    group.damage = Integer.parseInt(m.group(11));
+    int baseDamage = Integer.parseInt(m.group(11));
+    int damage = faction.equals("Immune System") ? baseDamage + boost : baseDamage;
+    group.damage = damage;
     group.attackType = m.group(12);
     group.initiative = Integer.parseInt(m.group(13));
     String modifiers = m.group(3);
@@ -77,35 +76,45 @@ public class Advent24 extends Advent {
 
   @Override
   protected String part1() {
+    playGame();
+    return "" + winnersAlive();
+  }
+
+  private void playGame() {
     Map<Group, Group> targets;
     while (bothFactionsAlive()) {
       targets = findTargets();
+      if (targets.isEmpty()) {
+        return;
+      }
       for (var pair : targets.entrySet()) {
         pair.getKey().attack(pair.getValue());
       }
     }
-    return "" + groups.stream()
-                      .filter(g -> g.units > 0)
-                      .mapToInt(g -> g.units)
-                      .sum();
   }
 
   private boolean bothFactionsAlive() {
-    long infection = groups.stream()
-                           .filter(g -> g.faction.equals("Infection"))
-                           .filter(g -> g.units > 0)
-                           .count();
-    long immune = groups.stream()
-                        .filter(g -> !g.faction.equals("Infection"))
-                        .filter(g -> g.units > 0)
-                        .count();
-    return infection > 0 && immune > 0;
+    return infectionCount() > 0 && immuneCount() > 0;
+  }
+
+  private long infectionCount() {
+    return groups.stream()
+                 .filter(g -> g.faction.equals("Infection"))
+                 .filter(Group::alive)
+                 .count();
+  }
+
+  private long immuneCount() {
+    return groups.stream()
+                 .filter(g -> !g.faction.equals("Infection"))
+                 .filter(Group::alive)
+                 .count();
   }
 
   private Map<Group, Group> findTargets() {
     Map<Group, Group> result = new TreeMap<>(Comparator.comparingInt((Group g) -> g.initiative).reversed());
     List<Group> eligibleGroups = groups.stream()
-                                       .filter(g -> g.units > 0)
+                                       .filter(Group::alive)
                                        .sorted(groupComparator)
                                        .collect(Collectors.toList());
     List<Group> eligibleTargets = new ArrayList<>(eligibleGroups);
@@ -123,8 +132,9 @@ public class Advent24 extends Advent {
 
   private Group findTarget(Group group, List<Group> targets) {
     return targets.stream()
-                  .filter(g -> g.units > 0)
+                  .filter(Group::alive)
                   .filter(g -> !g.faction.equals(group.faction))
+                  .filter(g -> group.damageTo(g) > 0)
                   .sorted(damageComparator(group))
                   .findFirst()
                   .orElse(null);
@@ -132,23 +142,35 @@ public class Advent24 extends Advent {
 
   private Comparator<Group> damageComparator(Group group) {
     return Comparator.comparingInt((Group g) -> group.damageTo(g))
-                     .thenComparingInt((Group g) -> g.units*g.damage)
+                     .thenComparingInt((Group g) -> g.effectivePower())
                      .thenComparingInt((Group g) -> g.initiative)
                      .reversed();
   }
 
-  private int effectivePower(Group group) {
+  private long winnersAlive() {
     return groups.stream()
-                 .filter(g -> !g.faction.equals(group.faction))
-                 .filter(g -> !g.immunities.contains(group.attackType))
-                 .mapToInt(g -> (g.weaknesses.contains(group.attackType) ? 2 : 1)*group.units*group.damage)
-                 .max()
-                 .orElse(0);
+    .filter(Group::alive)
+    .mapToInt(g -> g.units)
+    .sum();
   }
 
   @Override
   protected String part2() {
-    return "";
+    while (!winner().equals("Immune System")) {
+      ++boost;
+      parseInput();
+      playGame();
+    }
+    return "" + winnersAlive();
+  }
+
+  private String winner() {
+    List<String> factionsAlive = groups.stream()
+                                       .filter(Group::alive)
+                                       .map(g -> g.faction)
+                                       .distinct()
+                                       .collect(Collectors.toList());
+    return factionsAlive.size() > 1 ? "Deadlock" : factionsAlive.get(0);
   }
 
   class Group {
@@ -161,6 +183,10 @@ public class Advent24 extends Advent {
     List<String> weaknesses;
     List<String> immunities;
 
+    boolean alive() {
+      return units > 0;
+    }
+
     int damageTo(Group other) {
       if (other.immunities.contains(this.attackType)) return 0;
       int factor = other.weaknesses.contains(this.attackType) ? 2 : 1;
@@ -172,8 +198,10 @@ public class Advent24 extends Advent {
     }
 
     void attack(Group other) {
+      if (!this.alive()) return;
       int damage = this.damageTo(other);
       int killed = Math.min(damage / other.hp, other.units);
+      if (debug) sopl("Killed ", killed);
       other.units -= killed;
     }
 
