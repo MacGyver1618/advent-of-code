186f8e5922be7221c81be7688db5eaeb74030aee
Joni Malén <macgyver1618@gmail.com>
2021-12-03 16:07:29 +0200

    2016 Day 18 Python

---
+++ 2016/python/advent_18.py
@@ -0,0 +1,34 @@
+from advent_lib import *
+import collections as coll
+import functools as func
+import itertools as it
+import operator as oper
+import re
+import more_itertools as it2
+import numpy as np
+import sympy as sym
+
+inpt = lines(18)
+
+rows = inpt.copy()
+
+traps = [
+    "..^",
+    ".^^",
+    "^^.",
+    "^.."
+]
+
+def gen_next(line):
+    padded = "." + line + "."
+    return "".join(["^" if s in traps else "." for s in [padded[i-1:i+2] for i in range(1, len(padded)-1)]])
+
+while len(rows) < 40:
+    rows.append(gen_next(rows[-1]))
+
+print("Part 1:", "".join(rows).count("."))
+
+while len(rows) < 400_000:
+    rows.append(gen_next(rows[-1]))
+
+print("Part 2:", "".join(rows).count("."))
